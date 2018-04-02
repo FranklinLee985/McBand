7e71cdb045b5c50326fc4871d41ce582669afabe
WHenrYin44 <whenryin44@gmail.com>
2018-04-02 16:40:28 +0800

    Added a potential connecting port in account.html

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/db');
+
+function checkLogin(req,res,next){
+    if(!req.session.logInfo){
+        return res.redirect('/sign.html');
+    }
+    res.locals.logInfo = req.session.logInfo;
+    next();
+}
+
+function checkNotLogin(req,res,next){
+    if(req.session.logInfo){
+        return res.redirect('back');
+    }
+    next();
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/account.html', checkLogin,function(req, res, next) {
+  res.redirect('/users/' + res.locals.logInfo.name);
 });
 
+router.get('/users/',checkLogin,function(req, res, next) {
+  res.redirect('/users/' + res.locals.logInfo.name);
+});
+
+router.get('/users/:name',checkLogin,function(req, res, next) {
+	var por = '';
+	db.connect(function(){
+		db.getPortrait(res.locals.logInfo.email,por,function(){
+			db.disconnect();
+			console.log(por);
+			res.render('account',{portrait:por});
+		});
+	});
+
+  
+});
 module.exports = router;

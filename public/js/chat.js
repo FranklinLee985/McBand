e44515fb81399583aae0aa0b005557f07ddb5dbb
Linyhazel <32091308+Linyhazel@users.noreply.github.com>
2018-04-04 15:48:20 +0800

    chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,9 +2,13 @@ var socket = io();
 
 //emit chat message when btn is clicked
 $(document).ready(function() {
+	var usericon = document.getElementById('usericon').innerHTML;
     $('#senmesg').click(function() {
       	if($("#chat-input").val() != ""){
-			socket.emit("chat-message",$("#chat-input").val());
+			socket.emit("chat-message",{
+				msg: $("#chat-input").val(), 
+				usericonp: usericon
+			});
 			$("#chat-input").val("");
 		}
     });
@@ -12,21 +16,26 @@ $(document).ready(function() {
 
 //emit chat mesage when enter key is pressed
 $("#chat-input").keydown(function(event){
+	var usericon = document.getElementById('usericon').innerHTML;
+	//alert(usericon);
 	if (event.keyCode == 13){
 		event.preventDefault();
 		if($("#chat-input").val() != ""){
-			socket.emit("chat-message",$("#chat-input").val());
+			socket.emit("chat-message",{
+				msg: $("#chat-input").val(), 
+				usericonp: usericon
+			});
 			$("#chat-input").val("");
 		}
 	}
 });
 
 //receive chat messgae from server
-socket.on("chat-message",function(message){
+socket.on("chat-message",function(data){
 	var time = new Date();
-	$('#chatlogs').append(
-        "<div class='chat self'>" + "<div class='user-photo'>" + "<img src='images/person_1.jpg'>"
-        +"</div>" + "<p class='chat-message'>" + message + "</p>" + "</div>" + "<span class='grey-text' style='margin-left: 100;'>" + time.getHours() + ":" + time.getMinutes() + "</span>"
+	$('#chatlogs').append(                                        
+        "<div class='chat self'>" + "<div class='user-photo'>" + "<img src="+ data.usericonp +">"
+        +"</div>" + "<p class='chat-message'>" + data.msg + "</p>" + "</div>" + "<span class='grey-text' style='margin-left: 100;'>" + time.getHours() + ":" + time.getMinutes() + "</span>"
     );
 	// $("#chatlogs").append(message+"<br />")
 });
